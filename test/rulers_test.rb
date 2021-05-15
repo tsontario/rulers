971c0866a072e04af2dc8ea503a65d739b17e18c
Tim <tsontario@users.noreply.github.com>
2021-05-15 16:42:35 -0400

    First controller (#1)

--- test/rulers_test.rb
+++ test/rulers_test.rb
@@ -2,13 +2,21 @@
 
 require "test_helper"
 
+class TedController < Rulers::Controller
+  def think
+    "Whoah, man..."
+  end
+end
+
 class RulersTest < Minitest::Test
+
   def test_that_it_has_a_version_number
     refute_nil ::Rulers::VERSION
   end
 
-  def test_app_returns_success
-    env = { "PATH_INFO" => "/", "QUERY_STRING" => "" }
+  def test_new_controller_action
+    env = { "PATH_INFO" => "/ted/think", "QUERY_STRING" => "" }
     assert_equal(200, ::Rulers::App.new.call(env)[0])
+    assert_equal([TedController.new(nil).think], ::Rulers::App.new.call(env)[2])
   end
 end
