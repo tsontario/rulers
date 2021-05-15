51263d73cb7456dd3487b86317a32156aeefeb54
Timothy Smith <timothysmith@Tims-MacBook-Pro.local>
2021-05-15 13:10:10 -0400

    add minitest

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,5 +4,13 @@ require_relative "rulers/version"
 
 module Rulers
   class Error < StandardError; end
-  # Your code goes here...
+  class App
+    def call(env)
+      [
+        200,
+        { "Content-Type" => "text/html" },
+        ["Hello from Rulers"]
+      ]
+    end
+  end
 end
