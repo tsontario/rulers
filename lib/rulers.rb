fb0db4bcef4af0f38f79c4a6ff911256da11bfd3
Timothy Smith <timothysmith@Tims-MacBook-Pro.local>
2021-05-15 16:27:51 -0400

    Basic controller implementation

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -4,13 +4,33 @@ require_relative "rulers/version"
 
 module Rulers
   class Error < StandardError; end
+  class Controller
+    attr_reader(:env)
+
+    def initialize(env)
+      @env = env
+    end
+  end
+
   class App
     def call(env)
+      raise "foobar"
+      klass, act = controller_and_action(env)
+      # Forward action to target class. TODO: proper router so callers can't trigger arbitrary controller methods
+      text = klass.new(env).send(action)
       [
         200,
         { "Content-Type" => "text/html" },
-        ["Hello from Rulers"]
+        [text]
       ]
+    end
+
+    private
+
+    def controller_and_action(env)
+      _, controller, action, after = env["PATH_INFO"].split("/")
+      controller_class = Object.const_get("#{controller&.capitalize}Controller")
+      [ controller_class, action]
     end
   end
 end
