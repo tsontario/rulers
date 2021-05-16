f80bc4f7c56e21d7dee0950707a831a9d98cb2a3
Tim <tsontario@users.noreply.github.com>
2021-05-15 20:58:42 -0400

    Add render method to controller base class (#4)

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -23,11 +23,19 @@ module Rulers
     ).downcase
   end
 
+  require "erb"
   class Controller
     attr_reader(:env)
 
     def initialize(env)
       @env = env
+    end
+
+    # default binding is that of the caller
+    def render(name, b = binding())
+      template = "app/views/#{name}.html.erb"
+      e = ERB.new(File.read(template))
+      e.result(b)
     end
   end
 
