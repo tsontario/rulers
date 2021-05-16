24521208f81cb385d14347f51990623c53b901a8
Tim <tsontario@users.noreply.github.com>
2021-05-15 22:30:38 -0400

    Params wrapper of Rack::Request (#5)

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -31,6 +31,14 @@ module Rulers
       @env = env
     end
 
+    def request
+      @request ||= Rack::Request.new(env)
+    end
+
+    def params
+      request.params
+    end
+
     # default binding is that of the caller
     def render(name, b = binding())
       template = "app/views/#{name}.html.erb"
